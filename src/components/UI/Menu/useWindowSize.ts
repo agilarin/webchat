ad115184ff6b9c1896dbbc82057bd1f9d2784a9f
Vladimir <grivle@mail.ru>
2025-02-25 17:58:27 +0700

    add useWindowSize.ts

---
+++ src/components/UI/Menu/useWindowSize.ts
@@ -0,0 +1,22 @@
+import {useEffect, useState} from "react";
+import {debounce} from "@/utils/debounce.ts";
+
+
+function useWindowSize() {
+  const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
+
+
+  useEffect(() => {
+    const debouncedResizeHandler = debounce(() => {
+      setSize([window.innerWidth, window.innerHeight]);
+    }, 100);
+
+    window.addEventListener('resize', debouncedResizeHandler);
+    return () => window.removeEventListener('resize', debouncedResizeHandler);
+  }, []);
+
+
+  return size;
+}
+
+export default useWindowSize;
