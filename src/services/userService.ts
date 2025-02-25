da7fb6e524652092c5ae7e0c0eec1575924fff94
Vladimir <grivle@mail.ru>
2025-02-25 18:03:00 +0700

    update userService.ts

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,10 +1,27 @@
-import {collection, doc, getDoc} from "firebase/firestore";
+import {
+  collection, doc, getDoc,
+  updateDoc, serverTimestamp,
+  query, where, getDocs, onSnapshot
+} from "firebase/firestore";
 import {UserType} from "@/types";
 import {firestore} from "@/services/firebase.ts";
 
 
 class userService {
   usersRef = collection(firestore, "users");
+
+
+  async checkUsername(username: string) {
+    const usernameQuery = query(this.usersRef, where('username', "==", username))
+    try {
+      const snapshot = await getDocs(usernameQuery)
+      return snapshot.empty;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
 
   async getUser(userId: string) {
     const userRef = doc(this.usersRef, userId);
@@ -21,6 +38,41 @@ class userService {
       throw error;
     }
   }
+
+
+  async updateUser(userId: string, user: Partial<Omit<UserType, "createdAt" | "updatedAt" | "email">>) {
+    const userRef = doc(this.usersRef, userId);
+    try {
+      let usernameUnique;
+      if (user.username) {
+        usernameUnique = await this.checkUsername(user.username);
+      }
+      if (user.username && !usernameUnique) {
+        throw new Error("Username is already in use");
+      }
+
+      await updateDoc(userRef, {
+        ...user,
+        updatedAt: serverTimestamp(),
+      })
+
+
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  subscribeToUser(
+    userId: string,
+    callback: (snapshot: UserType) => void
+  ) {
+    const userRef = doc(this.usersRef, userId);
+    return onSnapshot(userRef, (snapshot) => {
+      callback(snapshot.data() as UserType);
+    });
+  }
+
 }
 
 
